c1f785c1bf81d5afa2fe77f491f8afd458af480d
mglauser <marco.glauser@gmail.com>
2016-07-21 10:28:06 +0200

    added moving auto location

--- static/js/script.js
+++ static/js/script.js
@@ -2,18 +2,41 @@ jQuery(document).ready(function($) {
 
     var map;
     var pokemons = {};
+    var position = {};
+    var positionMarker;
 
-    function initMap(position) {
-        console.log(position)
+    function initMap(initPosition) {
         map = new google.maps.Map(document.getElementById('map'), {
-            center: {lat: position.coords.latitude, lng: position.coords.longitude},
+            center: {lat: initPosition.coords.latitude, lng: initPosition.coords.longitude},
             zoom: 18
         });
-        setup_websocket();
-        startUpdate(position);
-        /*setInterval(function (position) {
-            startUpdate(position)
-        },30*1000)*/
+        setupPositionMarker();
+        /*setup_websocket();
+        startUpdate();
+        setInterval(startUpdate,50*1000)*/
+    }
+
+    function setupPositionMarker(){
+        console.log({position: {lat:position.coords.latitude,lng:position.coords.longitude}})
+        positionMarker = new google.maps.Marker({
+            position: {lat:position.coords.latitude,lng:position.coords.longitude},
+            title: 'You\'re here'
+        });
+        var image = {
+            url: '/static/images/pokeball.png',
+            // This marker is 20 pixels wide by 32 pixels high.
+            size: new google.maps.Size(96, 96),
+            // The anchor for this image is the base of the flagpole at (0, 32).
+            anchor: new google.maps.Point(48, 48)
+        };
+
+        positionMarker.setIcon(image);
+        positionMarker.setMap(map);
+        console.log('set on map')
+    }
+
+    function updatePositionMarker() {
+        positionMarker.setPosition({lat:position.coords.latitude,lng:position.coords.longitude})
     }
 
     function setup_websocket() {
@@ -54,9 +77,9 @@ jQuery(document).ready(function($) {
                 var image = {
                     url: spriteUrl,
                     // This marker is 20 pixels wide by 32 pixels high.
-                    size: new google.maps.Size(96, 96),
+                    size: new google.maps.Size(48, 48),
                     // The anchor for this image is the base of the flagpole at (0, 32).
-                    anchor: new google.maps.Point(48, 48)
+                    anchor: new google.maps.Point(24, 24)
                 };
 
                 marker.setIcon(image);
@@ -83,17 +106,26 @@ jQuery(document).ready(function($) {
         marker = null;
     }
 
-    function startUpdate(position){
+    function startUpdate(){
         random_component = Math.floor((Math.random() * 20*1000));
         setTimeout(function () {
             $.ajax('/pokeworld/test/?latitude='+position.coords.latitude+'&longitude='+position.coords.longitude)
         },random_component)
     }
 
+    function updatePosition(new_position) {
+        if (position.coords.latitude != new_position.coords.latitude && position.coords.longitude != new_position.coords.longitude){
+            console.log('new coordinates')
+            position = new_position
+            updatePositionMarker()
+        }
+    }
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function (position) {
-            initMap(position)
-        })
+        navigator.geolocation.getCurrentPosition(function (initPosition) {
+            position = initPosition;
+            initMap(initPosition)
+        });
+        navigator.geolocation.watchPosition(updatePosition);
     }
     else {
         initMap({coords:{latitude:0,longitude:0}});
