5355cf4ea10cbbe15b657092e419c354499fd3b4
mglauser <marco.glauser@gmail.com>
2016-07-22 06:18:44 +0200

    switched to official pgoapi

--- pokeworld/models.py
+++ pokeworld/models.py
@@ -3,5 +3,5 @@ from __future__ import unicode_literals
 from django.db import models
 
 class Pokemon(models.Model):
-    name = models.CharField(max_length=256)
-    number = models.IntegerField(db_index=True)
+    name = models.CharField(max_length=256,unique=True)
+    number = models.IntegerField(db_index=True,unique=True)
